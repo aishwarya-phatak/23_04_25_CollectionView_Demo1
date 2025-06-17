7cc91a8d3ea9709f0d054ce502ef8bd5909e17f3
aishwarya <ashp17star@gmail.com>
2025-06-17 17:00:03 +0530

    .

---
+++ 23_04_25_CollectionView_Demo/ProductCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCollectionViewCell.swift
+//  23_04_25_CollectionView_Demo
+//
+//  Created by Vishal Jagtap on 16/06/25.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var productTestImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    
+    }
+}
---
+++ 23_04_25_CollectionView_Demo/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  23_04_25_CollectionView_Demo
+//
+//  Created by Vishal Jagtap on 16/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var productCollectionView: UICollectionView!
+    let reuseIdentifierForCell = "ProductCollectionViewCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initViews()
+        registerCellWithCollectionView()
+    }
+    
+    func initViews(){
+        productCollectionView.delegate = self
+        productCollectionView.dataSource = self
+    }
+    
+    func registerCellWithCollectionView(){
+        let uiNib = UINib(nibName: reuseIdentifierForCell, bundle: nil)
+        self.productCollectionView.register(uiNib, forCellWithReuseIdentifier: reuseIdentifierForCell)
+    }
+}
+
+extension ViewController : UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let productCollectionViewCell = self.productCollectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifierForCell, for: indexPath) as! ProductCollectionViewCell
+        
+        return productCollectionViewCell
+    }
+}
+
+
+extension ViewController : UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = self.productCollectionView.frame.width/3
+        let cellHeight: CGFloat = cellWidth
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+}
