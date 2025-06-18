3aa97d35121eed40dfcf51051427c9c0a7ba8c2b
aishwarya <ashp17star@gmail.com>
2025-06-18 17:15:47 +0530

    .

---
+++ 23_04_25_CollectionView_Demo/MenuItemsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuItemsTableViewCell.swift
+//  23_04_25_CollectionView_Demo
+//
+//  Created by Vishal Jagtap on 18/06/25.
+//
+
+import UIKit
+
+class MenuItemsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
